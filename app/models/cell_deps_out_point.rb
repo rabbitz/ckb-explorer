32c9cdd23bcda647b1434960c43247b75f1b720c
Miles Zhang <mingchang555@hotmail.com>
2024-11-21 21:23:19 +0900

    fix api
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/models/cell_deps_out_point.rb
+++ app/models/cell_deps_out_point.rb
@@ -1,6 +1,8 @@
 class CellDepsOutPoint < ApplicationRecord
   belongs_to :contract, foreign_key: :deployed_cell_output_id, primary_key: :deployed_cell_output_id
   has_many :cell_dependencies, foreign_key: :contract_cell_id, primary_key: :contract_cell_id
+
+  scope :list_contract_cell_ids_by_contract, ->(contract_ids) { joins(:contract).where(contracts: { id: contract_ids }).pluck(:contract_cell_id) }
 end
 
 # == Schema Information
