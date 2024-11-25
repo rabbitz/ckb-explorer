1742b1b656f8f96e00ba75598195a220a0e8141f
Miles Zhang <mingchang555@hotmail.com>
2024-11-25 20:42:34 +0900

    fix
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/models/ckb_sync/new_node_data_processor.rb
+++ app/models/ckb_sync/new_node_data_processor.rb
@@ -59,7 +59,8 @@ module CkbSync
         inputs = @inputs = {}
         outputs = @outputs = {}
         outputs_data = @outputs_data = {}
-        @ckb_txs = build_ckb_transactions!(node_block, local_block, inputs, outputs, outputs_data).to_a
+        cell_deps = {}
+        @ckb_txs = build_ckb_transactions!(node_block, local_block, inputs, outputs, outputs_data, cell_deps).to_a
         benchmark :build_udts!, local_block, outputs, outputs_data
 
         tags = []
@@ -74,7 +75,7 @@ module CkbSync
         addrs_changes = Hash.new { |hash, key| hash[key] = {} }
 
         input_capacities, output_capacities = benchmark :build_cells_and_locks!, local_block, node_block, ckb_txs, inputs, outputs,
-                                                        tags, udt_address_ids, dao_address_ids, contained_udt_ids, contained_address_ids, addrs_changes, token_transfer_ckb_tx_ids
+                                                        tags, udt_address_ids, dao_address_ids, contained_udt_ids, contained_address_ids, addrs_changes, token_transfer_ckb_tx_ids, cell_deps
 
         # update explorer data
         benchmark :update_ckb_txs_rel_and_fee, ckb_txs, tags, input_capacities, output_capacities, udt_address_ids,
@@ -866,6 +867,34 @@ dao_address_ids, contained_udt_ids, contained_addr_ids
         end
       end
 
+      hash2index = {}
+      hash2id = {}
+      ckb_txs.each do |t|
+        hash2id["0#{t['tx_hash'][1..]}"] = t["id"]
+        hash2index["0#{t['tx_hash'][1..]}"] = t["tx_index"]
+      end
+
+      cell_deps_attrs = []
+      cell_deps.each do |tx_hash, cell_deps|
+        txid = hash2id[tx_hash]
+        tx_index = hash2index[tx_hash]
+
+        cell_deps.each do |cell_dep|
+          cell_deps_attrs <<
+            {
+              ckb_transaction_id: txid,
+              dep_type: cell_dep.dep_type,
+              contract_cell_id: CellOutput.find_by_pointer(cell_dep.out_point.tx_hash, cell_dep.out_point.index).id,
+              block_number: local_block.number,
+              tx_index:,
+            }
+        end
+      end
+      if cell_deps_attrs.present?
+        CellDependency.upsert_all(cell_deps_attrs,
+                                  unique_by: %i[ckb_transaction_id contract_cell_id dep_type])
+      end
+
       prev_outputs = nil
       build_cell_inputs(inputs, ckb_txs, local_block.id, cell_inputs_attributes, prev_cell_outputs_attributes,
                         input_capacities, tags, udt_address_ids, dao_address_ids, contained_udt_ids, contained_addr_ids,
@@ -1239,14 +1268,12 @@ _prev_outputs, index = nil)
       end
     end
 
-    def build_ckb_transactions!(node_block, local_block, inputs, outputs, outputs_data)
+    def build_ckb_transactions!(node_block, local_block, inputs, outputs, outputs_data, cell_deps)
       cycles = CkbSync::Api.instance.get_block_cycles node_block.header.hash
       ckb_transactions_attributes = []
       hashes = []
       header_deps = {}
-      cell_deps = {}
       witnesses = {}
-      hash2index = {}
       node_block.transactions.each_with_index do |tx, tx_index|
         attrs = ckb_transaction_attributes(local_block, tx, tx_index)
         if cycles
@@ -1254,7 +1281,6 @@ _prev_outputs, index = nil)
         end
         header_deps[tx.hash] = tx.header_deps
         cell_deps[tx.hash] = tx.cell_deps
-        hash2index[tx.hash] = tx_index
         witnesses[tx.hash] = tx.witnesses
         ckb_transactions_attributes << attrs
         hashes << tx.hash
@@ -1272,7 +1298,7 @@ _prev_outputs, index = nil)
       CkbTransaction.where("tx_hash IN (#{binary_hashes}) AND tx_status = 0").update_all tx_status: "committed"
 
       txs = CkbTransaction.upsert_all(ckb_transactions_attributes, unique_by: %i[tx_status tx_hash],
-                                                                   returning: %w(id tx_hash block_timestamp created_at))
+                                                                   returning: %w(id tx_hash tx_index block_timestamp created_at))
 
       if pending_txs.any?
         hash_to_pool_times = pending_txs.to_h
@@ -1312,27 +1338,6 @@ _prev_outputs, index = nil)
       if header_deps_attrs.present?
         HeaderDependency.upsert_all(header_deps_attrs,
                                     unique_by: %i[ckb_transaction_id index])
-      end
-
-      cell_deps_attrs = []
-      cell_deps.each do |tx_hash, cell_deps|
-        txid = hash2id[tx_hash]
-        tx_index = hash2index[tx_hash]
-
-        cell_deps.each do |cell_dep|
-          cell_deps_attrs <<
-            {
-              ckb_transaction_id: txid,
-              dep_type: cell_dep.dep_type,
-              contract_cell_id: CellOutput.find_by_pointer(cell_dep.out_point.tx_hash, cell_dep.out_point.index).id,
-              block_number: local_block.number,
-              tx_index:,
-            }
-        end
-      end
-      if cell_deps_attrs.present?
-        CellDependency.upsert_all(cell_deps_attrs,
-                                  unique_by: %i[ckb_transaction_id contract_cell_id dep_type])
       end
 
       # process witnesses
